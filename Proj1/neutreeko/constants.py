90067cbec1d0e64b268694b80b985897e160da55
Telmo <up201806821@fe.up.pt>
2021-03-13 21:15:13 +0000

    Updated design

--- Proj1/neutreeko/constants.py
+++ Proj1/neutreeko/constants.py
@@ -5,10 +5,9 @@ ROWS, COLS = 5, 5
 SQUARE_SIZE = WIDTH//COLS
 
 # rgb
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-WHITE = (200, 200, 200)
-BLACK = (0, 0, 0)
-BLUE = (0, 0, 255)
-GREY = (128,128,128)
-BLACK = (0,0,0)
+RED = (235, 235, 235)  #White
+GREEN = (104, 106, 135) #Purple suave
+WHITE = (118, 212, 230) 
+BLUE = (61, 61, 61) #Preto suave
+GREY = (128,128,128) 
+BLACK = (101, 184, 201)
